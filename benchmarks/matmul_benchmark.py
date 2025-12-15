744e5f1fe68fd9f69887178a304678d759d68f13
NotShrirang <shrirangmahajan123@gmail.com>
2025-12-15 22:51:57 +0530

    feat: enhance matrix multiplication benchmarks and add NumPy comparison

--- benchmarks/matmul_benchmark.py
+++ benchmarks/matmul_benchmark.py
@@ -1,5 +1,6 @@
 import timeit
 
+import numpy as np
 import torch
 import tensorax as ts
 
@@ -11,35 +12,53 @@ b_torch = torch.randn((3, 1024, 1024), device='cuda', dtype=torch.float32)
 a_t = ts.Tensor(a_torch.cpu().numpy(), dtype='float32', device='cuda')
 b_t = ts.Tensor(b_torch.cpu().numpy(), dtype='float32', device='cuda')
 
+a_np = a_torch.cpu().numpy()
+b_np = b_torch.cpu().numpy()
 
 # Benchmarking matmul with shared memory coalescing
 def matmul_shared_memory_coalesced():
+    torch.cuda.synchronize()
     c = a_t.matmul(b_t, method="shared_memory_coalesced")
+    torch.cuda.synchronize()
     return c
 
 # Benchmarking default matmul
 def matmul_default():
+    torch.cuda.synchronize()
     c = a_t.matmul(b_t, method="default")
+    torch.cuda.synchronize()
     return c
 
 # Benchmarking tiled matmul
 def matmul_tiled():
+    torch.cuda.synchronize()
     c = a_t.matmul(b_t, method="tiled")
+    torch.cuda.synchronize()
     return c
 
 # Benchmarking matmul with shared memory cache blocking
 def matmul_cache_blocking():
+    torch.cuda.synchronize()
     c = a_t.matmul(b_t, method="shared_memory_cache_blocking")
+    torch.cuda.synchronize()
     return c
 
 # Benchmarking matmul with 1D block tiling
 def matmul_1d_block_tiling():
+    torch.cuda.synchronize()
     c = a_t.matmul(b_t, method="block_tiling_1d")
+    torch.cuda.synchronize()
     return c
+
+def matmul_numpy():
+    c_np = np.matmul(a_np, b_np)
+    return c_np
 
 # Benchmarking PyTorch matmul
 def matmul_pytorch():
+    torch.cuda.synchronize()
     c = torch.matmul(a_torch, b_torch)
+    torch.cuda.synchronize()
     return c
 
 # Warm-up run
@@ -49,6 +68,7 @@ matmul_shared_memory_coalesced()
 matmul_tiled()
 matmul_cache_blocking()
 matmul_1d_block_tiling()
+matmul_numpy()
 matmul_pytorch()
 print("Warm-up done.")
 
@@ -68,6 +88,9 @@ print(f"Tiled matmul time over {times} runs: {time_tiled} seconds")
 
 time_1d_block_tiling = timeit.timeit(matmul_1d_block_tiling, number=times)
 print(f"Matmul with 1D block tiling time over {times} runs: {time_1d_block_tiling} seconds")
+
+time_numpy = timeit.timeit(matmul_numpy, number=times)
+print(f"Numpy matmul time over {times} runs: {time_numpy} seconds")
 
 time_pytorch = timeit.timeit(matmul_pytorch, number=times)
 print(f"PyTorch matmul time over {times} runs: {time_pytorch} seconds")
